888eb6f41781983d77d2431be29c5fe18d89c5ff
David Detweiler <daviddetweiler1@gmail.com>
2021-02-20 23:20:40 -0800

    Type importZombies

--- src/res/services/firebase.tsx
+++ src/res/services/firebase.tsx
@@ -30,28 +30,8 @@ interface ImportZombiesData {
   zombies: Zombie[]
 }
 
-const importZombies: (data: ImportZombiesData) => void = functions.httpsCallable('importZombies');
+type ImportZombies = (data: ImportZombiesData) => Promise<firebase.functions.HttpsCallableResult>;
+
+const importZombies: ImportZombies = functions.httpsCallable('importZombies');
 
 export { firestore, auth, storage, importZombies };
-
-// DAVID'S OLD CODE (delete if we don't need it anymore)
-/*
-function initFirebase() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCAnV-1BdHUGv3VwSZf-AoTVKsnZuaZK1w",
-    authDomain: "octopus-63e06.firebaseapp.com",
-    databaseURL: "https://octopus-63e06-default-rtdb.firebaseio.com",
-    projectId: "octopus-63e06",
-    storageBucket: "octopus-63e06.appspot.com",
-    messagingSenderId: "912650001024",
-    appId: "1:912650001024:web:dc1830879645063302e465",
-    measurementId: "G-2TD3DXEJ4W",
-  };
-
-
-  firebase.initializeApp(firebaseConfig);
-}
-
-
-
-*/
