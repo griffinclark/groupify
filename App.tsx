342cdbeaa6fa7cffacbc700ebb035cfbf906da33
Griffin Clark <32806921+GriffClark@users.noreply.github.com>
2021-02-20 19:35:52 -0800

    fixing "setting a timer" message

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import RootNavigation from "./src/res/root-navigation";
 
 export default function App() {
   LogBox.ignoreLogs(['source.uri should not be an empty string']); // This error pops up because of how we create the image view, but it isn't a big deal
+  LogBox.ignoreLogs(['Setting a timer']); // No it doesn't...
 
   return (
     <View style={globalStyles.defaultRootContainer}>
