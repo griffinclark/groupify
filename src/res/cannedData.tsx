3ef3d2fd7713f34718b20aaad64e96f635bac166
Griffin Clark <32806921+GriffClark@users.noreply.github.com>
2021-02-28 19:49:58 -0800

    adding more data

--- src/res/cannedData.tsx
+++ src/res/cannedData.tsx
@@ -3,38 +3,47 @@ export interface CannedEvent {
   startTime?: string; // this will be added later in the event creation process
   imageURL: string;
   description: string;
-  tags: string[]
+  tags: string[];
 }
 
-let cannedEvents: CannedEvent[] = [
+export const cannedEvents: CannedEvent[] = [
   {
     title: "City Tacos",
     imageURL:
       "https://diningonthe101.files.wordpress.com/2020/01/city-tacos-salsa-sign.jpg?w=1680",
-    description: "City Tacos is located in the Lumber Yard and offers inside or outdoor seating.  The outdoor tables come with little fire places which are inviting and comfortable. ",
-    tags: ["Mexican"]
+    description:
+      "City Tacos is located in the Lumber Yard and offers inside or outdoor seating.  The outdoor tables come with little fire places which are inviting and comfortable. ",
+    tags: ["Mexican"],
   },
   {
     title: "Modern Times Far West Lounge & Gelato 101",
-    imageURL: "https://diningonthe101.files.wordpress.com/2019/11/img_3832.jpg?w=1680",
+    imageURL:
+      "https://diningonthe101.files.wordpress.com/2019/11/img_3832.jpg?w=1680",
     description: "The food is all vegan.  A vegan brewery!",
-    tags: ["Healthy"]
+    tags: ["Healthy"],
   },
   {
-    title: "Karina’s Taco Shop    ",
-    imageURL: "https://diningonthe101.files.wordpress.com/2019/09/karinas-sign.jpg?w=1680",
-    description: "Inside Karina’s has a picture of their “Bomb” burrito.  It’s called the “Bomb” because every single person who has ever ordered one has died. ",
-    tags: ["Mexican"]
+    title: "Karina’s Taco Shop ",
+    imageURL:
+      "https://diningonthe101.files.wordpress.com/2019/09/karinas-sign.jpg?w=1680",
+    description:
+      "Inside Karina’s has a picture of their “Bomb” burrito.  It’s called the “Bomb” because every single person who has ever ordered one has died. ",
+    tags: ["Mexican"],
   },
   {
-    title:"Nectarine Grove    ",
-    imageURL: "https://diningonthe101.files.wordpress.com/2019/09/img_3698.jpg?w=1680",
+    title: "Nectarine Grove",
+    imageURL:
+      "https://diningonthe101.files.wordpress.com/2019/09/img_3698.jpg?w=1680",
     description: "Nectarine Grove is a small order-at-the-counter kinda place.",
-    tags: ["Healthy"]
-  }
+    tags: ["Healthy"],
+  },
+  {
+    title: "Coffee Coffee & Saint Archer Brewing Company",
+    imageURL:
+      "https://diningonthe101.files.wordpress.com/2019/08/img_3663.jpg?w=840",
+    description: "Coffee Coffee is right by the old Surfy Surfy which is now Bing Surfboards.  They took over the lease and still have a lot of Surfy Surfy stuff. ",
+    tags: ["Coffee"],
+  },
 ];
 
-let tagList =[
-  "Healthy",
-  "Mexican"
-]
+let tagList = ["Healthy", "Mexican", "Coffee"];
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,7 @@ import firebase from "firebase";
 import { FlatList } from "react-native-gesture-handler";
 import DataDisplay from "../organisms/DataDisplay";
 import Navbar from "../organisms/Navbar";
+import { cannedEvents } from "../res/cannedData";
 
 interface Props {
   navigation: any;
@@ -14,49 +15,50 @@ interface Props {
 
 export default function Home({ navigation }: Props) {
   const [feedData, setFeedData] = useState([]);
-  const [testData, setTestData] = useState([]);
+  const [testData, setTestData] = useState([{}]);
 
   // Used to grab dummy data from FB
   useEffect(() => {
-    // Grab all of the users that our logged in user is friends with, and all of the posts sent to the user
-    let testEvent = {
-      dateCreated: firebase.firestore.FieldValue.serverTimestamp(), // TODO not 100% sure this works
-      version: 0,
-      UID: "1",
-      title: "Climbing (ofc)",
-      endpointUID: "no endpoint UID",
-      creatorUID: "no creator UID",
-      type: "event", // TODO there should be a better way to figure out what type something is, but I don't know it off the top of my head
-      startTime: firebase.firestore.FieldValue.serverTimestamp(), // startTime == dateCreated for testing
-    };
+    // // Grab all of the users that our logged in user is friends with, and all of the posts sent to the user
+    // let testEvent = {
+    //   dateCreated: firebase.firestore.FieldValue.serverTimestamp(), // TODO not 100% sure this works
+    //   version: 0,
+    //   UID: "1",
+    //   title: "Climbing (ofc)",
+    //   endpointUID: "no endpoint UID",
+    //   creatorUID: "no creator UID",
+    //   type: "event", // TODO there should be a better way to figure out what type something is, but I don't know it off the top of my head
+    //   startTime: firebase.firestore.FieldValue.serverTimestamp(), // startTime == dateCreated for testing
+    // };
 
-    let testEvent2 = {
-      dateCreated: firebase.firestore.FieldValue.serverTimestamp(), // TODO not 100% sure this works
-      version: 0,
-      UID: "2",
-      title: "Climbing (ofc)",
-      endpointUID: "no endpoint UID",
-      creatorUID: "no creator UID",
-      type: "event", // TODO there should be a better way to figure out what type something is, but I don't know it off the top of my head
-      startTime: firebase.firestore.FieldValue.serverTimestamp(), // startTime == dateCreated for testing
-    };
+    // let testEvent2 = {
+    //   dateCreated: firebase.firestore.FieldValue.serverTimestamp(), // TODO not 100% sure this works
+    //   version: 0,
+    //   UID: "2",
+    //   title: "Climbing (ofc)",
+    //   endpointUID: "no endpoint UID",
+    //   creatorUID: "no creator UID",
+    //   type: "event", // TODO there should be a better way to figure out what type something is, but I don't know it off the top of my head
+    //   startTime: firebase.firestore.FieldValue.serverTimestamp(), // startTime == dateCreated for testing
+    // };
 
-    let testUser = {
-      dateCreated: firebase.firestore.FieldValue.serverTimestamp(), // TODO not 100% sure this works
-      version: 0,
-      type: "user",
-      UID: "0",
-      activated: true,
-      email: "gclark@munchkinlabs.us",
-      phoneNumber: "7608893464",
-      firstName: "Griffin",
-      lastName: "Clark",
-      username: "gclark00",
-      profileImageURL:
-        "https://ih1.redbubble.net/image.775821485.6679/mwo,x1000,ipad_2_snap-pad,750x1000,f8f8f8.u3.jpg",
-    };
+    // let testUser = {
+    //   dateCreated: firebase.firestore.FieldValue.serverTimestamp(), // TODO not 100% sure this works
+    //   version: 0,
+    //   type: "user",
+    //   UID: "0",
+    //   activated: true,
+    //   email: "gclark@munchkinlabs.us",
+    //   phoneNumber: "7608893464",
+    //   firstName: "Griffin",
+    //   lastName: "Clark",
+    //   username: "gclark00",
+    //   profileImageURL:
+    //     "https://ih1.redbubble.net/image.775821485.6679/mwo,x1000,ipad_2_snap-pad,750x1000,f8f8f8.u3.jpg",
+    // };
 
-    setTestData([testEvent, testUser, testEvent2]);
+    // setTestData([testEvent, testUser, testEvent2]);
+    setTestData(cannedEvents)
   }, []);
 
   return (
