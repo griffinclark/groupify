cfe2cc8b9c95bfd5c8decb67d45abcfb9409181e
David Detweiler <daviddetweiler1@gmail.com>
2021-04-02 13:53:50 -0700

    Implement Twilio API secret retrieval

--- amplify/backend/function/pushBroadcast/src/index.py
+++ amplify/backend/function/pushBroadcast/src/index.py
@@ -1,9 +1,20 @@
 import json
+import boto3
 
 def handler(event, context):
   print('received event:')
   print(event)
-  
+
+  if event['httpMethod'] != 'POST':
+    return {
+      'statusCode': 501
+    }
+
+  sm = boto3.client('secretsmanager')
+  key = eval(sm.get_secret_value(SecretId='twilio-api')['SecretString'])
+  sid = key['sid']
+  auth_token = key['auth_token']  
+
   return {
       'statusCode': 200,
       'headers': {
@@ -11,5 +22,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'body': ''
   }
