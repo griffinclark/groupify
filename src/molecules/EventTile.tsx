6ca52e8be41a4b9b7a903c23e4b00c30a87da64c
Justin Quach <justinquedible@gmail.com>
2021-04-05 20:03:01 -0700

    Remove extra whitespace on home screen

--- src/molecules/EventTile.tsx
+++ src/molecules/EventTile.tsx
@@ -48,11 +48,11 @@ export default function EventTile({
   return (
     <View style={styles.rootContainer}>
       <Text style={globalStyles.title}> {title} </Text>
-      <View style={styles.rowConatiner}>
+      {/* <View style={styles.rowConatiner}>
         <View style={styles.profileImageContainer}>
           <SquareImageDisplay imageURI={imageURL} />
         </View>
-      </View>
+      </View> */}
       <View style={globalStyles.miniSpacer} />
       <View style={styles.rowConatiner}>
         <View style={globalStyles.defaultColumnContainer}>
@@ -63,8 +63,7 @@ export default function EventTile({
             <Text><Text style={{fontWeight: "bold"}}>Time: </Text>{time}</Text>
             <Text><Text style={{fontWeight: "bold"}}>Location: </Text>{location}</Text>
             <View style={globalStyles.miniSpacer} />
-            {friends != null && (<Text style={{fontWeight: "bold"}}>Invited Friends: </Text>
-)}
+            {friends != null && (<Text style={{fontWeight: "bold"}}>Invited Friends: </Text>)}
             <Text>{friends}</Text>
             {displayButton == true && (<Button title={"select"} onPress={()=>{navigation.navigate("SelectFriends", {data: {tagData: tagData, eventData: {
               title: title,
@@ -82,7 +81,7 @@ export default function EventTile({
 
 let styles = StyleSheet.create({
   rootContainer: {
-    height: 500,
+    // height: 500,
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
