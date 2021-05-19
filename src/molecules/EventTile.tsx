ece69923e8f0df75eb8d8f0ba118b63ebad2ce7e
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-19 11:09:30 -0700

    Refactored EventTile

--- src/molecules/EventTile.tsx
+++ src/molecules/EventTile.tsx
@@ -1,61 +1,42 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
-import {
-  DK_PURPLE,
-  GREY_3,
-  GREY_5,
-  POST_SPACING,
-  TEST_HIGH_CONTRAST,
-  TEST_IMAGE_URL,
-  WHITE,
-} from "../res/styles/Colors";
-import { template } from "@babel/core";
-import { Contact, Event } from "./../res/dataModels";
-// import { styles } from "../atoms/Screen";
-import { Title } from "../atoms/Title";
-import { FriendList } from "../organisms/FriendList";
-
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { DK_PURPLE, GREY_5, POST_SPACING } from '../res/styles/Colors';
+import { Event } from './../res/dataModels';
+import { FriendList } from '../organisms/FriendList';
 
 interface EventTileProps {
-  event: Event,
-  displayButton: boolean,
-  navigation: any
+  event: Event;
 }
 
-export function EventTile({
-  event,
-  displayButton,
-  navigation
-}: EventTileProps) {
-
+export const EventTile: React.FC<EventTileProps> = ({ event }: EventTileProps) => {
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>{event.title}</Text>
       <Text>{event.description}</Text>
       <View style={styles.infoContainer}>
         <View style={styles.infoItem}>
-        <Text style={styles.label}>When</Text>
-        <Text>{event.date}</Text>
-        <Text>{event.time}</Text>
+          <Text style={styles.label}>When</Text>
+          <Text>{event.date}</Text>
+          <Text>{event.time}</Text>
         </View>
         <View style={styles.infoItem}>
-        <Text style={styles.label}>Where</Text>
-        <Text>{event.location}</Text>
+          <Text style={styles.label}>Where</Text>
+          <Text>{event.location}</Text>
         </View>
       </View>
-      <FriendList title="Invited Friends" friends={event.friends}/>
+      <FriendList title="Invited Friends" friends={event.friends} />
     </View>
   );
-}
+};
 
-let styles = StyleSheet.create({
+const styles = StyleSheet.create({
   rootContainer: {
     // height: 500,
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: GREY_5,
     borderRadius: 10,
     marginTop: POST_SPACING,
@@ -64,20 +45,20 @@ let styles = StyleSheet.create({
   },
   infoContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: '100%',
     marginHorizontal: 20,
     marginVertical: 10,
   },
   infoItem: {
-    margin: 5
+    margin: 5,
   },
   title: {
     fontSize: 25,
     color: DK_PURPLE,
     fontWeight: 'bold',
-    marginVertical: 10
+    marginVertical: 10,
   },
   label: {
     fontWeight: 'bold',
--- src/molecules/ImageSelector.tsx
+++ src/molecules/ImageSelector.tsx
@@ -1,28 +1,30 @@
-import * as Permissions from "expo-permissions";
-import * as ImagePicker from "expo-image-picker";
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Button,} from "react-native";
-import {CircularImageDisplay} from "../atoms/AtomsExports";
-import { SECONDARY } from "../res/styles/Colors";
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { CircularImageDisplay } from '../atoms/AtomsExports';
+import { SECONDARY } from '../res/styles/Colors';
 
 interface Props {
   imageURI: string;
-  setImageURI: any;
+  setImageURI: (ev: string) => void;
 }
 
-export function ImageSelector({ imageURI, setImageURI }: Props) {
+export const ImageSelector: React.FC<Props> = ({ imageURI, setImageURI }: Props) => {
   // TODO rebuild this to use atomic design
-  const [localImageURI, setLocalImageURI] = useState("");
+  const [localImageURI, setLocalImageURI] = useState('');
 
   useEffect(() => {
     if (imageURI) {
       setLocalImageURI(imageURI);
     } else {
-      setLocalImageURI("https://static.wikia.nocookie.net/disney/images/0/03/Profile_-_Winnie_the_Pooh.png/revision/latest?cb=20200510194837")
+      setLocalImageURI(
+        'https://static.wikia.nocookie.net/disney/images/0/03/Profile_-_Winnie_the_Pooh.png/revision/latest?cb=20200510194837',
+      );
     }
   });
 
-  let selectPhoto = async () => {
+  const selectPhoto = async () => {
     await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
     const { uri } = await ImagePicker.launchImageLibraryAsync({
       aspect: [1, 1],
@@ -35,21 +37,20 @@ export function ImageSelector({ imageURI, setImageURI }: Props) {
   };
 
   return (
-    <View >
+    <View>
       {/* IMAGE */}
       <View style={styles.imagePreviewContainer}>
         <CircularImageDisplay imageURI={localImageURI} />
       </View>
       {/* BUTTON */}
       <View style={styles.chooseImageButtonContainer}>
-        <Button title={"Select Photo"} color={SECONDARY} onPress={selectPhoto} />
+        <Button title={'Select Photo'} color={SECONDARY} onPress={selectPhoto} />
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
- 
   imagePreviewContainer: {
     height: 200,
     width: 200,
