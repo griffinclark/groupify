0ab631497adbbd5e9701eeb40a49084b7b708325
Justin Quach <justinquedible@gmail.com>
2021-05-19 19:30:01 -0700

    Refactor SquareImageDisplay

--- src/atoms/SquareImageDisplay.tsx
+++ src/atoms/SquareImageDisplay.tsx
@@ -4,25 +4,22 @@ import { Image, StyleSheet } from "react-native";
 // XXX should this be a TouchableOpacity?
 
 interface Props {
-    imageURI: string;
-    size ?: number
-  }0
+  imageURI: string;
+}
 
-  export function SquareImageDisplay({imageURI, size}: Props) {
-      return(
-          <Image
-          style={styles.imagePreviewContainer}
-          source={{
-              uri: imageURI
-          }}
-          />
-      )
-  }
+export const SquareImageDisplay: React.FC<Props> = ({ imageURI }: Props) => {
+  return (
+    <Image
+      style={ styles.imagePreviewContainer }
+      source={{ uri: imageURI }}
+    />
+  );
+};
 
-  let styles = StyleSheet.create({
-    imagePreviewContainer: {
-        height: "100%",
-        width: "100%",
-        borderRadius: 0,
-      },
-  })
+const styles = StyleSheet.create({
+  imagePreviewContainer: {
+    height: '100%',
+    width: '100%',
+    borderRadius: 0,
+  },
+});
