fa80a4a6b405be2a92f2d4cd70f26f6ae68348a6
Justin Quach <justinquedible@gmail.com>
2021-05-20 01:24:40 -0700

    Update Navbar.tsx

--- src/molecules/Navbar.tsx
+++ src/molecules/Navbar.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-export const Navbar: React.FC = (props) => {
+interface Props {
+}
+
+export const Navbar: React.FC = (props: React.PropsWithChildren<Props>) => {
   return <View style={styles.nav}>{props.children}</View>;
 };
 
 const styles = StyleSheet.create({
   nav: {
     height: 50,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent: "space-between",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
     paddingHorizontal: 10,
-    paddingBottom: 10 
-  }
+    paddingBottom: 10,
+  },
 });
