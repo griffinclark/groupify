40a644c48a36a4ee9580eeebc8e8aedb8df8c53d
Justin Quach <justinquedible@gmail.com>
2021-05-20 02:33:00 -0700

    Update UserTile.tsx

--- src/molecules/UserTile.tsx
+++ src/molecules/UserTile.tsx
@@ -1,25 +1,26 @@
-import React, { useState } from "react";
-import { globalStyles } from "./../res/styles/GlobalStyles";
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
-import { POST_SPACING } from "../res/styles/Colors";
-import {CheckBox, CircularImageDisplay} from "../atoms/AtomsExports";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState } from 'react';
+import { globalStyles } from '../res/styles/GlobalStyles';
+import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { POST_SPACING } from '../res/styles/Colors';
+import { CheckBox, CircularImageDisplay } from '../atoms/AtomsExports';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-interface Props {
+interface UserTileProps {
   username: string;
   imageURL: string;
-  addUser?: any; // if there's a list of users, add the user by username when the checkbox is checked
-  removeUser?: any;
+  addUser?: (username: string) => void; // if there's a list of users, add the user by username when the checkbox is checked
+  removeUser?: (username: string) => void;
   createEvent?: boolean;
 }
-export function UserTile({
+
+export const UserTile: React.FC<UserTileProps> = ({
   username,
   imageURL,
   addUser,
   removeUser,
   createEvent,
-}: Props) {
+}: UserTileProps) => {
   const [checked, setChecked] = useState(false);
   return (
     <View style={styles.rootContainer}>
@@ -29,7 +30,7 @@ export function UserTile({
       <View>
         <Text style={globalStyles.title}> {username} </Text>
       </View>
-      {addUser && (
+      {addUser && removeUser && (
         <View style={styles.checkboxContainer}>
           <CheckBox
             isSelected={checked}
@@ -37,52 +38,51 @@ export function UserTile({
               setChecked(!checked);
               if (checked == true) {
                 addUser(username);
-                console.log("added");
+                console.log('added');
               } else {
-                removeUser(checked); // TODO fix
-                console.log("removed");
+                removeUser(username); // TODO fix
+                console.log('removed');
               }
             }}
           />
         </View>
       )}
       {createEvent && (
-        <TouchableOpacity style={styles.createEventButton} onPress={()=>{console.log("Event!")}}><Text>Create Event</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.createEventButton} onPress={()=>{console.log('Event!')}}>
+          <Text>Create Event</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
-}
+};
 
-let styles = StyleSheet.create({
+const styles = StyleSheet.create({
   rootContainer: {
     height: 75,
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
     // backgroundColor: TEST_HIGH_CONTRAST,
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: POST_SPACING,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     paddingLeft: 20,
     paddingRight: 20,
-    borderBottomWidth: 1
-
+    borderBottomWidth: 1,
   },
   profileImageContainer: {
-    display: "flex",
+    display: 'flex',
     height: 50,
     width: 50,
     // This isn't the best way to get the image where we need it, but...
     // position: "absolute",
     // left: 10,
   },
-
   checkboxContainer: {
-    position: "absolute",
+    position: 'absolute',
     right: 10,
   },
-
   createEventButton: {
-    alignSelf:"flex-end"
-  }
+    alignSelf:'flex-end',
+  },
 });
