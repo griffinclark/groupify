1331fb820051092858d16620c4032aa318781410
Justin Quach <justinquedible@gmail.com>
2021-05-19 18:41:11 -0700

    Refactor Screen.tsx

--- src/atoms/Screen.tsx
+++ src/atoms/Screen.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from "react-native"
-import { WHITE } from "../res/styles/Colors";
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WHITE } from '../res/styles/Colors';
 
 interface ScreenProps {
-    style?: object //TODO: more specific type?
+  style?: object //TODO: more specific type?
 }
 
 export const Screen: React.FC<ScreenProps> = (props) => {
-    return (
-        <SafeAreaView style={[styles.screen, props.style]}>
-            {props.children}
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={[styles.screen, props.style]}>
+      {props.children}
+    </SafeAreaView>
+  );
 }
 
 export const styles = StyleSheet.create({
-    screen: {
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        backgroundColor: WHITE,
-        flexDirection: "column",
-        padding: 5
-    }
+  screen: {
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    backgroundColor: WHITE,
+    flexDirection: "column",
+    padding: 5
+  }
 });
