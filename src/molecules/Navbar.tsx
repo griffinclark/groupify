f7d930f6eae9494ea16e137617cabf0df1238c4c
Justin Quach <justinquedible@gmail.com>
2021-05-20 01:11:13 -0700

    Update Navbar.tsx

--- src/molecules/Navbar.tsx
+++ src/molecules/Navbar.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, View } from "react-native";
 
 export const Navbar: React.FC = (props) => {
   return <View style={styles.nav}>{props.children}</View>;
-}
+};
 
 const styles = StyleSheet.create({
   nav: {
