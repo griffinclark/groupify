8e6caadcb20d2c43a4d7e05a7a7056f92901c266
Justin Quach <justinquedible@gmail.com>
2021-05-19 21:13:12 -0700

    Update Tag.tsx

--- src/atoms/Tag.tsx
+++ src/atoms/Tag.tsx
@@ -7,45 +7,41 @@ interface Props {
   selectedTags: string[];
 }
 
-export const Tag: React.FC<Props> = ({title, setSelectedTags, selectedTags}: Props) => {
+export const Tag: React.FC<Props> = ({ title, setSelectedTags, selectedTags }: Props) => {
   const [pressed, setPressed] = useState(false);
 
   const tagPressed = () => {
     // TODO test to make sure this works as intended
-    setPressed(!pressed) // change whether the tag is pressed or not
+    setPressed(!pressed); // change whether the tag is pressed or not
     if (!pressed) {
       // add to array
       setSelectedTags((selectedTags: string[]) => [...selectedTags, title]);
-    }
-    else {
+    } else {
       // remove from array
-      selectedTags.filter(
-        (item, pos) => {
-          if (item == title) {
-            selectedTags.splice(pos, 1);
-            setSelectedTags(selectedTags);
-          }
+      selectedTags.filter((item, pos) => {
+        if (item == title) {
+          selectedTags.splice(pos, 1);
+          setSelectedTags(selectedTags);
         }
-      );
+      });
     }
   };
 
   const getColor = () => {
     if (pressed) {
       return 'green';
-    }
-    else {
+    } else {
       return 'gray';
     }
   };
 
   return (
     <View>
-      <Button 
-      title={title}
-      onPress={tagPressed}
-      color={getColor()}
-      // TODO change text color to make tags look like Tinder and Bumble
+      <Button
+        title={title}
+        onPress={tagPressed}
+        color={getColor()}
+        // TODO change text color to make tags look like Tinder and Bumble
       />
     </View>
   );
