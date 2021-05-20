4659fcb6db7edc9dd5559da9fbe762227d71184a
Justin Quach <justinquedible@gmail.com>
2021-05-19 22:50:47 -0700

    Update Title.tsx

--- src/atoms/Title.tsx
+++ src/atoms/Title.tsx
@@ -3,24 +3,21 @@ import { Text, StyleSheet } from 'react-native';
 import { DK_PURPLE } from '../res/styles/Colors';
 
 interface TitleProps {
-    style?: object
+  style?: object
 }
-export const Title: React.FC<TitleProps> = (props) => {
-    return (
-        <Text style={[styles.title, props.style]}>
-            {props.children}
-        </Text>
-    )
+
+export const Title: React.FC<TitleProps> = (props: React.PropsWithChildren<TitleProps>) => {
+  return <Text style={[styles.title, props.style]}>{props.children}</Text>;
 }
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 30, 
-        margin: 15,
-        // fontSize: 45,
-        // margin: 40,
-        color: DK_PURPLE,
-        textAlign: 'center',
-        fontWeight: 'bold',
-    }
-})
+  title: {
+    fontSize: 30, 
+    margin: 15,
+    // fontSize: 45,
+    // margin: 40,
+    color: DK_PURPLE,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
+});
