0f90b3c129d28ade02b9bee229dc102ae1b0e4dd
Justin Quach <justinquedible@gmail.com>
2021-05-20 20:58:56 -0700

    Delete UserDisplay.tsx
    This organism file was unused.

--- src/organisms/OrganismsExports.tsx
+++ src/organisms/OrganismsExports.tsx
@@ -1,9 +1,7 @@
 import {DataDisplay} from './DataDisplay'
 import {FriendList} from './FriendList'
-import {UserDisplay} from './UserDisplay'
 
 export {
     DataDisplay,
     FriendList,
-    UserDisplay
 }
