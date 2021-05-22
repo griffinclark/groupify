b942285dbbb9673c622b536ebc165e1f6f8004bd
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-21 22:33:44 -0700

    button refactor

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,64 +1,56 @@
-import React, { FormEvent } from "react";
-import { 
-    Button as ReactButton,
-    NativeSyntheticEvent, 
-    NativeTouchEvent,
-    StyleSheet,
-    Text,
-    View
-} from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { GREY_3, GREY_4, LT_PURPLE, WHITE } from "../res/styles/Colors";
+import React from 'react';
+import { NativeSyntheticEvent, NativeTouchEvent, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { GREY_3, GREY_4, LT_PURPLE, WHITE } from 'res/styles/Colors';
 
 interface ButtonProps {
-    onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void,
-    title: string,
-    disabled?: boolean
+  onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void;
+  title: string;
+  disabled?: boolean;
 }
-export const Button = ({onPress, title, disabled = false}: ButtonProps) => {
-    return (
-        <View style={buttonStyles.container}>
-            <TouchableOpacity
-                onPress={onPress}
-                disabled={disabled}
-                style={[buttonStyles.button, 
-                    disabled ? buttonStyles.disabledButton: buttonStyles.enabledButton]}
-            >
-                <Text style={[buttonStyles.text, 
-                disabled ? buttonStyles.disabledButton : buttonStyles.enabledButton]}>{title}</Text>
-            </TouchableOpacity>
-        </View>
-    )
+export const Button: React.FC<ButtonProps> = ({ onPress, title, disabled = false }: ButtonProps) => {
+  return (
+    <View style={buttonStyles.container}>
+      <TouchableOpacity
+        onPress={onPress}
+        disabled={disabled}
+        style={[buttonStyles.button, disabled ? buttonStyles.disabledButton : buttonStyles.enabledButton]}
+      >
+        <Text style={[buttonStyles.text, disabled ? buttonStyles.disabledButton : buttonStyles.enabledButton]}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
-
 export const buttonStyles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        width: '100%',
-        // borderColor: 'red',
-        // borderWidth: 2,
-        // borderStyle: 'solid'
-    },
-    button: {
-        borderRadius: 20,
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        marginVertical: 10,
-        marginHorizontal: 'auto',
-        minWidth: 150,
-        alignItems: 'center'
-    },
-    text: {
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
-    enabledButton: {
-        backgroundColor: LT_PURPLE,
-        color: WHITE,
-    },
-    disabledButton: {
-        backgroundColor: GREY_4,
-        color: GREY_3
-    }
-})
+  container: {
+    alignItems: 'center',
+    width: '100%',
+    // borderColor: 'red',
+    // borderWidth: 2,
+    // borderStyle: 'solid'
+  },
+  button: {
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 10,
+    marginHorizontal: 'auto',
+    minWidth: 150,
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  enabledButton: {
+    backgroundColor: LT_PURPLE,
+    color: WHITE,
+  },
+  disabledButton: {
+    backgroundColor: GREY_4,
+    color: GREY_3,
+  },
+});
