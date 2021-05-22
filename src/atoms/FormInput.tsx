3ce355ed32033c8b7974df844ce1956497aabb4d
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-21 22:37:42 -0700

    FormInput refactor

--- src/atoms/FormInput.tsx
+++ src/atoms/FormInput.tsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-import { GREY_5 } from '../res/styles/Colors';
+import { GREY_5 } from 'res/styles/Colors';
 
 interface FormProps {
-    onChangeText: React.Dispatch<string>;
-    label?: string;
-    placeholder?: string;
-    secureTextEntry?: boolean
+  onChangeText: React.Dispatch<string>;
+  label?: string;
+  placeholder?: string;
+  secureTextEntry?: boolean;
 }
 export const FormInput: React.FC<FormProps> = ({
-    onChangeText, 
-    label,
-    placeholder, 
-    secureTextEntry=false
-}) => {
-    return (
-        <View style={styles.wrapper}>
-            <Text>{label}</Text>
-            <TextInput
-                style={styles.input}
-                placeholder={placeholder}
-                onChangeText={onChangeText}
-                secureTextEntry={secureTextEntry}
-            />
-        </View>
-    )
-}
+  onChangeText,
+  label,
+  placeholder,
+  secureTextEntry = false,
+}: FormProps) => {
+  return (
+    <View style={styles.wrapper}>
+      <Text>{label}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    wrapper: {
-        marginHorizontal: 20,
-        marginVertical: 10
-    },
-    input: {
-        backgroundColor: GREY_5,
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 5
-    }
-})
+  wrapper: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  input: {
+    backgroundColor: GREY_5,
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+});
