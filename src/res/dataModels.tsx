80d5d4ab0beb26e710d33ca5b3c7dd87142de96c
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-27 09:57:32 -0700

    Deleted a bunch of stuff

--- src/res/dataModels.tsx
+++ src/res/dataModels.tsx
@@ -5,18 +5,7 @@ export interface Profile {
   lastName?: string; // some zombie accounts might not be made using a last name
   username?: string; // zombie accounts won't have usernames
   profileImageURL: string; // zombie accounts won't have profile photos
-  type: string; // this is a hack
 }
-
-// TODO: Share!!!
-export interface User {
-  dateCreated: firestore.Timestamp | firestore.FieldValue;
-  version: number;
-  UID: string;
-  activated: boolean;
-  profile: Profile;
-}
-
 export interface Event {
   uuid: string;
   title: string;
@@ -27,29 +16,6 @@ export interface Event {
   date: string;
   time: string;
   location: string;
-}
-
-export interface FriendRecord {
-  targetUID: string;
-  dateCreated: firestore.Timestamp | firestore.FieldValue;
-  version: number;
-}
-
-export interface EventInviteRecord {
-  dateCreated: string;
-  version: number;
-  UID: string;
-  targetUID: string;
-  ownerUID: string;
-  eventUID: string;
-}
-
-export interface Endpoint {
-  tags: string[];
-  image: string;
-  location: string; // google maps link
-  title: string;
-  description: string;
 }
 
 export interface Contact {
