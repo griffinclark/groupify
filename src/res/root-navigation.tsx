09e241bdaca5b769f86a279d130fdc8249d9822d
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-27 16:08:08 -0700

    deleted EventDetails screen

--- src/res/root-navigation.tsx
+++ src/res/root-navigation.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
 import { NavigationContainer, ParamListBase } from '@react-navigation/native';
-import React, { ReactNode } from 'react';
 import { Welcome } from '../screens/Welcome';
 import { Home } from '../screens/Home';
 import { SelectFriends } from '../screens/SelectFriends';
 import { CreateCustomEvent } from '../screens/CreateCustomEvent';
-import { EventDetails } from '../screens/EventDetails';
 import { CreateAccount } from '../screens/CreateAccount';
 import { LogIn } from '../screens/LogIn';
 import { ImportContacts } from '../screens/ImportContacts';
@@ -58,7 +57,6 @@ export const RootNavigation: React.FC<RootProps> = ({ initialRoute }: RootProps)
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="SelectFriends" component={SelectFriends} options={{ headerShown: false }} />
         <Stack.Screen name="CreateCustomEvent" component={CreateCustomEvent} options={{ headerShown: false }} />
-        <Stack.Screen name="EventDetails" component={EventDetails} options={{ headerShown: false }} />
         <Stack.Screen name="ImportContacts" component={ImportContacts} options={{ headerShown: false }} />
         <Stack.Screen name="SendMessage" component={SendMessage} options={{ headerShown: false }} />
       </Stack.Navigator>
