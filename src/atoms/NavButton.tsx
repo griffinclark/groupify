5586cc16a7b9bfa3121465492ae5b81e5b546a52
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-27 08:39:13 -0700

    NavButton part 2

--- src/atoms/NavButton.tsx
+++ src/atoms/NavButton.tsx
@@ -1,32 +1,29 @@
-import React from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Text, StyleSheet, NativeSyntheticEvent, NativeTouchEvent } from "react-native";
-import { LT_PURPLE, WHITE } from "../res/styles/Colors";
+import React from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Text, StyleSheet, NativeSyntheticEvent, NativeTouchEvent } from 'react-native';
+import { LT_PURPLE, WHITE } from '../res/styles/Colors';
 
 interface NavProps {
-    onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void,
-    title: string;
+  onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void;
+  title: string;
 }
-export const NavButton: React.FC<NavProps> = ({onPress, title}) => {
-    return (
-        <TouchableOpacity
-        style={styles.button}
-        onPress={onPress}
-      >
-        <Text style={styles.text}>{title}</Text>
-      </TouchableOpacity>
-    )
+export const NavButton: React.FC<NavProps> = ({ onPress, title }: NavProps) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {
-      backgroundColor: LT_PURPLE,
-      borderRadius: 20,
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-    },
-    text: {
-      fontWeight: 'bold',
-      color: WHITE
-    }
-  });
+  button: {
+    backgroundColor: LT_PURPLE,
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: WHITE,
+  },
+});
