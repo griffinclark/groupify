3ed196a768ee99bbd481c15f8af8c59ecf8eaa5e
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-05-28 17:53:33 -0700

    CHanged height prop name

--- src/atoms/MultiLineTextInput.tsx
+++ src/atoms/MultiLineTextInput.tsx
@@ -3,17 +3,17 @@ import { View, StyleSheet, TextInput } from 'react-native';
 import { WHITE } from '../res/styles/Colors';
 
 interface Props {
-  inputText: string; // this is a reference to where the data is being stored in the parent function
-  setText: (ev: string) => void; // this is a callback to the parent function to pass back the text that typed into this field
+  inputText: string;
+  setText: (ev: string) => void;
   placeholder: string;
-  style?: number;
+  height?: number;
 }
 
-export const MultiLineTextInput: React.FC<Props> = ({ inputText, setText, placeholder, style }: Props) => {
+export const MultiLineTextInput: React.FC<Props> = ({ inputText, setText, placeholder, height }: Props) => {
   return (
     <View>
       <TextInput
-        style={[styles.textInput, { height: `${style}` }]} // style has to be build here instead of in a stylesheet so we can dynamically set the height
+        style={[styles.textInput, { height: `${height}` }]}
         placeholder={placeholder}
         multiline={true}
         onChangeText={(text) => setText(text)}
