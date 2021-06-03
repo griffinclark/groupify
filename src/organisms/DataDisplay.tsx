841bbc494503ecdd2ae4e3591de1b992d8be4048
Ty Matson <80430068+tymatson109@users.noreply.github.com>
2021-06-03 09:07:01 -0700

    Just the DataDisplay file

--- src/organisms/DataDisplay.tsx
+++ src/organisms/DataDisplay.tsx
@@ -1,41 +1,23 @@
-import React from "react";
-import { View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import {EventTile} from "./../molecules/MoleculesExports";
-import { Contact, Event } from "./../res/dataModels";
+import React from 'react';
+import { View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { EventTile } from '../molecules/MoleculesExports';
+import { Event } from '../res/dataModels';
 
 interface Props {
   data: Event[];
-  navigation: any;
-  onSelect?: any;
-  displayButton: boolean
-  selectedTags?: object
 }
 
-// Adds a newline at the end of each friend in the array
-const formatFriends = (friends: Contact[]) => {
-  if (friends !== undefined) {
-    return friends.map(friend => friend + "\n");
-  }
-}
-
-export function DataDisplay({ data, navigation, displayButton }: Props) {
+export const DataDisplay: React.FC<Props> = ({ data }: Props) => {
   return (
     <View>
       <FlatList
         data={data}
         renderItem={({ item }) => {
-          return (
-            <EventTile
-              event={item}
-              displayButton={displayButton}
-              navigation={navigation}
-            />
-          );
+          return <EventTile event={item} />;
         }}
         keyExtractor={(item) => item.uuid}
-
       />
     </View>
   );
-}
+};
