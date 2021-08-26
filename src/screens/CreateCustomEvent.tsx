f782d033268c2e5dfad3b0bf1118092e51385373
tymatson109 <80430068+tymatson109@users.noreply.github.com>
2021-08-25 19:48:28 -0700

    Fixed another bug

--- src/screens/CreateCustomEvent.tsx
+++ src/screens/CreateCustomEvent.tsx
@@ -143,11 +143,7 @@ export const CreateCustomEvent: React.FC<Props> = ({ navigation, route }: Props)
             <Text style={styles.title}>New Plan</Text>
           </View>
           <View>{loadPhoto(photo)}</View>
-          <MeepForm
-            InputList={inputFields}
-            updatedValues={(value) => setValues(value)}
-            fullDate={(date) => setFullDate(date)}
-          >
+          <MeepForm InputList={inputFields} updatedValues={(value) => setValues(value)}>
             <FormButton title="Next" onPress={() => onFormSubmit(updatedValues)} />
           </MeepForm>
         </ScrollView>
