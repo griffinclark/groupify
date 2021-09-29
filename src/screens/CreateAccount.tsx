c0cc15e00d5b7dcee751e1388c568886bf5a8e63
tymatson109 <80430068+tymatson109@users.noreply.github.com>
2021-09-29 14:09:03 -0700

    Eslint

--- src/screens/CreateAccount.tsx
+++ src/screens/CreateAccount.tsx
@@ -1,5 +1,5 @@
 import { RoutePropParams } from '../res/root-navigation';
-import { Dimensions, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { Auth } from 'aws-amplify';
 import { Screen, FormInput, Button, Alert } from '../atoms/AtomsExports';
