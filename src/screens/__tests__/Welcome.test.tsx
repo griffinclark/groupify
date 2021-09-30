126e6133327edd893d3097d8b8d71baadcd286f7
Phil <phetzel89@gmail.com>
2021-09-29 20:57:20 -0700

    Pull request commit

--- src/screens/__tests__/Welcome.test.tsx
+++ src/screens/__tests__/Welcome.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import { Welcome } from '../Welcome';
 
 const mockProp = {
@@ -12,24 +12,19 @@ const mockProp = {
 
 describe('Welcome Screen', () => {
   const { getAllByText } = render(<Welcome {...mockProp} />);
-  const loginButton = getAllByText('Log In');
-  const signupButton = getAllByText('Sign Up');
 
   it('renders correctly', () => {
     /* eslint-disable */
     const text = getAllByText("Don't have an account?");
     /* eslint-enable */
+    const loginButton = getAllByText('Log In');
+    const signupButton = getAllByText('Sign Up');
 
     expect(text).toHaveLength(1);
     expect(loginButton).toHaveLength(1);
     expect(signupButton).toHaveLength(1);
   });
 
-  it('navigates to login page', () => {
-    console.log(loginButton);
-
-    // fireEvent.press(loginButton);
-  });
-
+  // it('navigates to login page', () => {});
   // it('navigates to create account', () => {});
 });
