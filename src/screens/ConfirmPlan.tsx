e0e9e907cf542be0743d10451b8e6093bc3fb651
SamikshaGaikwad <57677234+SamikshaGaikwad@users.noreply.github.com>
2021-09-30 14:07:22 -0400

    Update ConfirmPlan.tsx

--- src/screens/ConfirmPlan.tsx
+++ src/screens/ConfirmPlan.tsx
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Platform, KeyboardAvoidingView, FlatList } from 'react-native';
-import { Screen, Title, MeepForm, MultiLineTextInput, TwoButtonAlert } from '../atoms/AtomsExports';
-import { AppText } from '../atoms/AppText';
-import { TEAL, GREY_8, WHITE } from '../res/styles/Colors';
-import { AntDesign } from '@expo/vector-icons';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import { AppText, BottomButton, MeepForm, TwoButtonAlert, Navbar, MultiLineTextInput } from '../atoms/AtomsExports';
+import { KeyboardAvoidingView, Platform, StyleSheet, View, FlatList } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import Constants from 'expo-constants';
+import { GREY_8, TEAL } from '../res/styles/Colors';
 import { RoutePropParams } from '../res/root-navigation';
-import { Event, Contact } from '../res/dataModels';
-import { Icon } from 'react-native-elements';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Contact } from 'expo-contacts';
+import { PhoneNumberFormat, PhoneNumberUtil } from 'google-libphonenumber';
 import { API, Auth, DataStore } from 'aws-amplify';
+import { formatDatabaseDate, formatDatabaseTime } from '../res/utilFunctions';
 import { Plan, Status, Invitee, User } from '../models';
 import { sendPushNotification } from '../res/notifications';
-import { formatDatabaseDate, formatDatabaseTime } from '../res/utilFunctions';
-import { PhoneNumberFormat, PhoneNumberUtil } from 'google-libphonenumber';
 
 interface Props {
   navigation: {
@@ -22,71 +19,95 @@ interface Props {
     goBack: () => void;
   };
   route: RoutePropParams;
+  friend: Contact;
 }
 
 export const ConfirmPlan: React.FC<Props> = ({ navigation, route }: Props) => {
-  const event: Event = route.params.data.eventData;
+  const event = route.params.data.eventData;
   const currentUser: User = route.params.currentUser;
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [planTitle] = useState('');
-  const [planAddress] = useState('');
-  const [planDate] = useState('');
-  const [planTime] = useState('');
-  const [planDescription] = useState('');
-  const [message, setMessage] = useState<string>('Loading Message...');
+  const [name, setName] = useState<string>('');
+  const [, setDate] = useState<string>('');
+  const [, setTime] = useState<string>('');
+  const [, setDescription] = useState<string>('');
+  const [, setLocation] = useState<string>('');
+  const [, setPhoto] = useState<string>('');
+  const [, setDisabled] = useState<boolean>(true);
+  const [message, setMessage] = useState<string>('');
   const [editMessage, setEditMessage] = useState<boolean | undefined>(false);
-  const [error, setError] = useState<string | undefined>();
-  const [updatedValues, setUpdatedValues] = useState<{
-    eventName: string | undefined;
-    eventDate: string | undefined;
-    eventTime: string | undefined;
-    eventLocation: string | undefined;
-    eventDescription: string | undefined;
-  }>({
-    eventName: planTitle,
-    eventDate: planDate,
-    eventTime: planTime,
-    eventLocation: planAddress,
-    eventDescription: planDescription,
-  });
+  const [, setIsLoading] = useState<boolean>(false);
+
+  // Check if required fields are full
+  useEffect(() => {
+    checkDisabled();
+  }, [name]);
+
+  // Update location or photo
+  useEffect(() => {
+    if (route.params.data && route.params.data.eventData.location) {
+      setLocation(route.params.data.eventData.location);
+    }
+    if (route.params.data && route.params.data.eventData.imageURL) {
+      setPhoto(route.params.data.eventData.imageURL);
+    }
+  }, [route.params.data]);
+
+  const checkDisabled = () => {
+    if (name) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
   const inputFields: {
     title: string;
     placeholder: string;
-    settings?: string;
-    value?: string;
+    settings: string;
+    value: string;
+    func: React.Dispatch<React.SetStateAction<string>>;
+    disabled: boolean;
   }[] = [
     {
-      title: 'Place Name *',
-      placeholder: 'name',
+      title: 'Plan Name *',
+      placeholder: '',
       settings: 'default',
-      value: route.params.data ? route.params.data.eventData.title : '',
+      value: event.title,
+      func: setName,
+      disabled: true,
     },
     {
       title: 'Date *',
       placeholder: 'MM/DD/YYYY',
-      settings: 'date',
-      value: route.params.data ? route.params.data.eventData.date : '',
+      settings: 'default',
+      value: event.date,
+      func: setDate,
+      disabled: true,
     },
     {
       title: 'Time *',
       placeholder: 'H:MM PM',
-      settings: 'time',
-      value: route.params.data ? route.params.data.eventData.time : '',
+      settings: 'default',
+      value: event.time,
+      func: setTime,
+      disabled: true,
     },
     {
-      title: 'Location',
-      placeholder: 'address',
+      title: 'Description',
+      placeholder: '',
       settings: 'default',
-      value: route.params.data ? route.params.data.eventData.location : '',
+      value: event.description,
+      func: setDescription,
+      disabled: true,
     },
     {
-      title: 'Description *',
-      placeholder: 'description',
+      title: 'Address',
+      placeholder: '',
       settings: 'default',
-      value: route.params.data ? route.params.data.eventData.description : '',
+      value: event.location,
+      func: setLocation,
+      disabled: true,
     },
   ];
-
   const getUserName = async (): Promise<string> => {
     const userInfo = await Auth.currentUserInfo();
     return userInfo.attributes.name;
@@ -150,34 +171,6 @@ export const ConfirmPlan: React.FC<Props> = ({ navigation, route }: Props) => {
     });
     const obj = { attendees: attendees, content: message };
     console.log(await API.post('broadcastsApi', '/broadcasts', { body: obj }));
-  };
-
-  const setValues = (value: { title: string; value: string | undefined }[]) => {
-    const values = {
-      eventName: value[0].value,
-      eventDate: value[1].value,
-      eventTime: value[2].value,
-      eventLocation: value[3].value,
-      eventDescription: value[4].value,
-    };
-    if (!values.eventName) {
-      setError('Please add a name to your plan');
-      return;
-    }
-    if (!values.eventLocation) {
-      setError('Please add a location to your plan');
-      return;
-    }
-    if (!values.eventDescription) {
-      setError('Please add a description to your plan');
-      return;
-    }
-    route.params.data.eventData.title = values.eventName;
-    route.params.data.eventData.description = values.eventDescription;
-    route.params.data.eventData.location = values.eventLocation;
-    route.params.data.eventData.date = values.eventDate;
-    route.params.data.eventData.time = values.eventTime;
-    setUpdatedValues(values);
   };
 
   const storeInvitees = async () => {
@@ -257,169 +250,123 @@ export const ConfirmPlan: React.FC<Props> = ({ navigation, route }: Props) => {
   interface renderContactProps {
     item: Contact;
   }
-
   const contactList = ({ item }: renderContactProps) => {
     return (
-      <View key={item.id} style={styles.nameContainer}>
-        <View style={styles.bubble}>
-          <Icon size={30} color={'white'} name="check" type="entypo" />
+      <View style={styles.container}>
+        <View style={styles.nameContainer}>
+          <View style={styles.bubble}>
+            <AppText style={{ fontSize: 20 }}>{item.name.slice(0, 1)}</AppText>
+          </View>
+          <View>
+            <AppText style={styles.name}>{item.name}</AppText>
+          </View>
         </View>
-        <AppText style={styles.name}>{item.name}</AppText>
       </View>
     );
   };
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <Screen style={{ backgroundColor: WHITE }}>
-        <View style={styles.navbar}>
-          <AntDesign name="left" type="font-awesome" size={30} onPress={() => navigation.goBack()} />
-          <Title>Confirm</Title>
-          <AppText style={{ color: 'white' }}>blank</AppText>
-        </View>
-        <ScrollView>
-          <View>
-            <AppText style={styles.details}>
-              Almost done! Please confirm all details are correct, and that you’ve invited all you want. You can always
-              come back and edit this event at a later time.
-            </AppText>
-            <MeepForm InputList={inputFields} updatedValues={(value) => setValues(value)}></MeepForm>
-            <AppText style={styles.details}>Invitees</AppText>
-            <FlatList
-              data={event.contacts}
-              renderItem={contactList}
-              ListEmptyComponent={() => (
-                <View style={styles.title}>
-                  <AppText>No Contacts Invited</AppText>
-                </View>
-              )}
-              style={event.friends.length !== 0 ? { maxHeight: '20%' } : { maxHeight: '42%' }}
-            />
-            <AppText style={styles.details}>Contacts invited, who will receive a text message:</AppText>
-            <View style={styles.message}>
-              <MultiLineTextInput enabled={editMessage} inputText={message} setText={setMessage} placeholder={''} />
-            </View>
-            <TouchableOpacity onPress={() => setEditMessage(!editMessage)}>
-              <AppText style={styles.details}>Edit Note</AppText>
-            </TouchableOpacity>
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: 'white' }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      {/* <Screen> */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+          paddingTop: Constants.statusBarHeight,
+        }}
+      >
+        <Navbar location={'PlanCreate'} navigation={navigation} title={'Confirm'} />
+        <AppText style={styles.titleText}>
+          Almost done! Please confirm all details are correct, and that you’ve invited all you want. You can always come
+          back and edit this event at a later time.
+        </AppText>
+        {/* <View>{loadPhoto(photo)}</View> */}
+        <View style={{ flexGrow: 1 }}>
+          <MeepForm inputList={inputFields}></MeepForm>
+          <AppText style={styles.details}>Invitees</AppText>
 
-            <FlatList
-              data={event.contacts}
-              renderItem={contactList}
-              ListEmptyComponent={() => (
-                <View style={styles.title}>
-                  <AppText>No Contacts Invited</AppText>
-                </View>
-              )}
-              style={event.friends.length !== 0 ? { maxHeight: '20%' } : { maxHeight: '42%' }}
-            />
+          <View style={styles.contactsContainer}>
+            <FlatList data={event.contacts} renderItem={contactList} />
           </View>
-        </ScrollView>
-        <TouchableOpacity onPress={createConfirmAlert}>
-          <AppText style={[styles.navText, { backgroundColor: TEAL, color: WHITE }]}>Confirm and Create Event</AppText>
-        </TouchableOpacity>
-      </Screen>
+
+          <AppText style={styles.details}>Contacts invited, who will receive a text message:</AppText>
+          <View style={styles.message}>
+            <MultiLineTextInput enabled={editMessage} inputText={event.message} setText={setMessage} placeholder={''} />
+          </View>
+          <TouchableOpacity onPress={() => setEditMessage(!editMessage)}>
+            <AppText style={styles.mapText}>Edit Note</AppText>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.contactsContainer}>
+          <FlatList data={event.contacts} renderItem={contactList} />
+        </View>
+
+        <BottomButton title="Confirm and Create Event" onPress={createConfirmAlert} />
+      </ScrollView>
+      {/* </Screen> */}
     </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  image: {
-    height: 200,
-    width: '100%',
-  },
-  sphere: {
-    backgroundColor: TEAL,
-    width: 40,
-    height: 40,
-    borderRadius: 40,
-    margin: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '400',
+  mapText: {
     color: TEAL,
-    flexWrap: 'wrap',
-    maxWidth: 250,
-    textAlign: 'right',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 15,
-  },
-  hostName: {
-    fontSize: 20,
-    fontWeight: '400',
-  },
-  descTitle: {
-    fontWeight: '400',
-    fontSize: 12,
-    color: GREY_8,
-    marginVertical: 5,
-  },
-  hostContainer: {
-    flex: 1,
-  },
-  detailsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    flex: 1,
-  },
-  description: {
-    flex: 1,
-  },
-  container: {
-    flex: 5,
-    marginHorizontal: 25,
-    marginVertical: 20,
-  },
-  inviteeList: {
-    flex: 1,
-  },
-  viewAll: {
-    color: TEAL,
-  },
-  planResponse: {
-    flexDirection: 'row',
-    width: '50%',
-    flex: 1,
-    alignItems: 'flex-end',
-  },
-  initial: {
-    fontSize: 14,
-    fontWeight: '700',
-    color: 'white',
-  },
-  navbar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginHorizontal: 20,
+    fontSize: 16,
+    marginLeft: 10,
   },
   details: {
     fontSize: 16,
-    marginLeft: 20,
+    fontWeight: '500',
+    lineHeight: 22,
+    margin: 20,
   },
-  bubble: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#31A59F',
+  titleText: {
+    fontSize: 20,
+    lineHeight: 23,
+    textAlign: 'center',
+    margin: 20,
+  },
+  contactsContainer: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: 80,
     alignItems: 'center',
-    paddingVertical: 16,
-    borderRadius: 50,
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
   },
   name: {
     fontSize: 15,
     marginLeft: 10,
+    flexWrap: 'wrap',
+    maxWidth: 220,
+  },
+  phone: {
+    fontSize: 12,
+    marginLeft: 10,
+    color: GREY_8,
+  },
+  bubble: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#C4C4C4',
+    alignItems: 'center',
+    paddingVertical: 18,
+    borderRadius: 50,
   },
   nameContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 20,
   },
   message: {
     borderWidth: 1,
@@ -429,18 +376,5 @@ const styles = StyleSheet.create({
     borderColor: '#BE8C2C',
     margin: 15,
     maxHeight: '40%',
-  },
-  navText: {
-    fontSize: 24,
-    padding: 10,
-    textAlign: 'center',
-    alignSelf: 'center',
-    width: '100%',
-    height: 75,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 30,
   },
 });
