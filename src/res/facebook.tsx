72e49fbab70cb6fdea039633bbd848b9be9e0456
andrei-sebald <andreisebald@gmail.com>
2021-12-21 10:14:51 -0800

    minor variable changes

--- src/res/facebook.tsx
+++ src/res/facebook.tsx
@@ -2,11 +2,13 @@ import * as Facebook from 'expo-facebook'
 //Asks for permission for facebook to track user data, used in App.tsx
 const fbInit= async ()=> {
     await Facebook.initializeAsync({appId:'693164164986653',appName:"Munchkin Labs"})
-    let response = await Facebook.getPermissionsAsync()
+    const response = await Facebook.getPermissionsAsync()
+    let finalResponse= response
     if (response.status=="undetermined"){
-        response = await Facebook.requestPermissionsAsync()
+        const newResponse = await Facebook.requestPermissionsAsync()
+        finalResponse= newResponse
     }
-    else if (response.granted==true){
+    else if (finalResponse.granted==true){
         console.log("The user HAS granted data permissions for facebook")
         await Facebook.setAdvertiserTrackingEnabledAsync(true)
     }
