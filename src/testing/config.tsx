a40fc93fe06c94276b4ae1a354730b80cd2fe6b8
winifer _mt <winifer.mt2306@gmail.com>
2021-12-24 08:08:50 -0800

    upgrade sdk version

--- src/testing/config.tsx
+++ src/testing/config.tsx
@@ -14,7 +14,7 @@ jest.mock('react-native-reanimated', () => {
 });
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 // Folders to mock
 jest.mock('../res/storageFunctions');
