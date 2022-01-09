4df33ad22478832eb86ceeb5052bafe1d1467267
bearbobs <kapoor.anuj0@gmail.com>
2022-01-09 21:16:33 +0530

    :zap: Added native reload for e2e tests

--- e2e/checkWelcomeScreen.e2e.js
+++ e2e/checkWelcomeScreen.e2e.js
@@ -9,6 +9,10 @@ describe('WelcomeScreen', () => {
     await reloadApp();
   });
 
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
   it('should have welcome screen', async () => {
     await expect(element(by.id('WelcomeScreen'))).toBeVisible();
   });
