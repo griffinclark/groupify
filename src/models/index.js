9fea9cb082415db57ee055c77549592febd43a09
Ubuntu <ubuntu@ip-172-31-0-191.us-east-2.compute.internal>
2022-01-20 06:11:56 +0000

    :zap: added missing package

--- src/models/index.js
+++ src/models/index.js
@@ -2,19 +2,27 @@
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
+const Sender = {
+  "USER": "USER",
+  "NOTIFICATIONPANEL": "NOTIFICATIONPANEL"
+};
+
 const Status = {
   "PENDING": "PENDING",
   "ACCEPTED": "ACCEPTED",
   "DECLINED": "DECLINED"
 };
 
-const { Plan, User, Availability, PlanArbitration, Invitee } = initSchema(schema);
+const { NotificationFromTo, Notification, User, Availability, Plan, PlanArbitration, Invitee } = initSchema(schema);
 
 export {
-  Plan,
+  NotificationFromTo,
+  Notification,
   User,
   Availability,
+  Plan,
   PlanArbitration,
   Invitee,
+  Sender,
   Status
 };
