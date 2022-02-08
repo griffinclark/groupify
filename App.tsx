d8363c34eb3d5c2b00be1dcc9e62a7916c3f5db2
winifer _mt <winifer.mt2306@gmail.com>
2022-02-08 01:21:39 -0800

    update eslint typo

--- App.tsx
+++ App.tsx
@@ -26,12 +26,13 @@ export const App: React.FC = () => {
   const [userID, setUserID] = useState('');
   LogBox.ignoreLogs([
     '"requestPermissionsAsync()" is now deprecated.',
-    `"AsyncStorage·has·been·extracted·from·react-native·core·and·will·be·removed·in·a·future·release.·It·can·now·be·installed·and·imported·from·'@react-native-async-storage/async-storage'·instead·of·'react-native'.·See·https://github.com/react-native-async-storage/async-storage"`,
+    `AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage`,
     'FirebaseAnalytics.setCurrentScreen` is deprecated.',
     'Non-serializable values were found in the navigation state',
     'source.uri should not be an empty string',
     'Setting a timer',
-    `"[Unhandled·promise·rejection:·TypeError:·undefined·is·not·an·object·(evaluating·'userInfo.attributes.phone_number')]·"`  ]);
+    `[Unhandled promise rejection: TypeError: undefined is not an object (evaluating 'userInfo.attributes.phone_number')]`  
+  ]);
 
   useEffect(() => {
     Hub.listen('auth', (event) => {
