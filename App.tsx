9cd5b9e2ba7eb8864a6bd8c89cf4143bae00d7f9
winifer _mt <winifer.mt2306@gmail.com>
2022-02-08 01:42:05 -0800

    disable eslint quote errors

--- App.tsx
+++ App.tsx
@@ -25,6 +25,7 @@ export const App: React.FC = () => {
   const [initalScreen, setInitialScreen] = useState('');
   const [userID, setUserID] = useState('');
   LogBox.ignoreLogs([
+    // eslint-disable-next-line quotes
     `'requestPermissionsAsync()' is now deprecated.`,
     // eslint-disable-next-line quotes
     `AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage`,
