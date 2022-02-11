f71df604561ee25e3bccd694b5ee0591b186ce56
Ernest Nnamdi <ernest@Ernests-MacBook-Air.local>
2022-02-11 20:20:44 +0100

    first commit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -14,7 +14,6 @@ import * as Location from 'expo-location';
 import { RoutePropParams } from '../res/root-navigation';
 import { LocationAccuracy } from 'expo-location';
 import { Header } from '../atoms/Header';
-import { ImportContactTile } from '../atoms/ImportContactTile';
 
 export interface Props {
   navigation: {
