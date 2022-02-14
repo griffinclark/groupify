cf745ee6d86208715a33e7eb8f3a391f4a42714e
winifer _mt <winifer.mt2306@gmail.com>
2022-02-13 22:18:21 -0800

    eslint fix

--- App.tsx
+++ App.tsx
@@ -13,7 +13,6 @@ import * as Notifications from 'expo-notifications';
 import { facebookInit } from './src/res/facebookTracking';
 import * as Font from 'expo-font';
 import { Jost_400Regular, Jost_500Medium, Jost_600SemiBold, Jost_700Bold } from '@expo-google-fonts/jost';
-import { getCurrentUser } from './src/res/utilFunctions';
 
 Amplify.configure(awsconfig);
 
