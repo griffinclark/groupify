c2db8bb09a132e7d6207f96fdb4d4a34ee4db220
winifer _mt <winifer.mt2306@gmail.com>
2022-03-27 08:23:07 -0700

    select time state

--- src/atoms/AvailabilityItem.tsx
+++ src/atoms/AvailabilityItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { JOST } from '../res/styles/Fonts';
 import { TimeBox } from './TimeBox';
@@ -7,28 +7,43 @@ interface Props {
   day: string;
 }
 
-const data1 = ['6.00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'];
+const data1 = ['6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'];
 const data2 = ['12:00', '12:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30'];
 export const AvailabilityItem: React.FC<Props> = ({ day }: Props) => {
+  const [availableTime, setAvailableTime] = useState<any>({
+    Mon: [],
+  });
+
+  const handlePress = (item: string, removed: boolean) => {
+    if(removed) {
+      availableTime.Mon = availableTime.Mon.filter((i: string) => i !== item);
+    }
+    else {
+      availableTime.Mon.push(item);
+    }
+
+    setAvailableTime({...availableTime});
+  }
+
   return (
     <ScrollView style={{ marginHorizontal: 20, marginTop: 24, height: 400 }}>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <View>
           <Text style={styles.text}>Early</Text>
           {data1.map((item, index) => (
-            <TimeBox day={day} item={item} key={index} />
+            <TimeBox day={day} item={item} key={index} onPress={handlePress} />
           ))}
         </View>
         <View>
           <Text style={styles.text}>Mid Day</Text>
           {data2.map((item, index) => (
-            <TimeBox day={day} item={item} key={index} />
+            <TimeBox day={day} item={item} key={index} onPress={handlePress} />
           ))}
         </View>
         <View>
           <Text style={styles.text}>Evening</Text>
           {data1.map((item, index) => (
-            <TimeBox day={day} item={item} key={index} />
+            <TimeBox day={day} item={item} key={index} onPress={handlePress} />
           ))}
         </View>
       </View>
